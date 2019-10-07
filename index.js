e408f8f209ba700b2249bf4ebdcf4d136205f5d6
Shane M <srm2486@yahoo.com>
2019-10-07 22:55:25 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,10 @@
+var greeting = "Hello, everybody!";
+
+var specialGuest = "Neil deGrasse Tyson"
+
+var greetSpecialGuest = "" + specialGuest + "!";
+
+var topic = "space";
+
+var conversation = `${topic}`;
+
