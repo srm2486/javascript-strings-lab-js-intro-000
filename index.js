f57c518076e2ac3886f9021b6ef0d5d27014b806
Shane M <srm2486@yahoo.com>
2019-10-07 22:51:23 +0000

    Automatically backed up by Learn

--- index.js
+++
@@ -1,9 +0,0 @@
-var greeting = "Hello, everybody!";
-
-var specialGuest = " Hello, Neil deGrasse Tyson";
-
-var greetSpecialGuest = "Hello, " + specialGuest + "!";
-
-var topic = 'Let\'s talk about space';
-
-var conversation = `${topic}`;
